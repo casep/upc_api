6f129703342f9e62ed306fa5f5c2dea73e05f7fc
Max <carlos.sepulveda.farfan@gmail.com>
2024-09-19 18:28:55 +0100

    Fixed error handling for invalid code, fixed Healthcheck CMD, fixed README order

--- app/app.py
+++ app/app.py
@@ -6,9 +6,16 @@ from sqlalchemy import create_engine
 app = Flask(__name__)
 
 # GET endpoint
+@app.route('/', methods=['GET'])
+def get_tasks():
+    return "App OK"
 @app.get('/api/get/<id>')
 def get_record(id):
-    disk_engine=create_engine('sqlite:///my_lite_store.db')
-    upc_corpus=pd.read_sql_query('SELECT * FROM ean WHERE ean = '+id,disk_engine)
-    return upc_corpus['name'][0]
+    try:
+        disk_engine=create_engine('sqlite:///my_lite_store.db')
+        upc_corpus=pd.read_sql_query('SELECT * FROM ean WHERE ean = '+id,disk_engine)
+        item = upc_corpus['name'][0] + "\n"
+        return item
+    except:
+        return("Not valid code\n")
 
